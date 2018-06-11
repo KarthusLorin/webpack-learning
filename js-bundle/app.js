8018b752aab85707afe7cf4cf7d01b8d547341b9
KarthusLorin <karthuslorin@gmail.com>
2018-06-11 23:39:46 +0800

    add js-bundle

---
+++ js-bundle/app.js
@@ -0,0 +1,13 @@
+// es6 module
+import sum from './sum'
+
+// commonJS
+let minus = require('./minus')
+
+// amd
+require(['./muti'], function (muti) {
+    console.log('muti(2, 3) =', muti(2, 3))
+})
+
+console.log('sum(23, 24) =', sum(23, 24))
+console.log('minus(24, 17) =', minus(24, 17))
